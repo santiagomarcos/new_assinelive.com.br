c194b3d1e292726d3b63d014d1b1abc3ee108002
Leandro Souza Da Silva <leandro.dasilvati7@gmail.com>
2019-04-02 18:22:04 -0300

    implements image banner carousel

---
+++ resources/views/components/_header.blade.php
@@ -0,0 +1,25 @@
+<header class="header-content">
+    <div class="banner-content">
+        <img src="https://i.imgur.com/9IhCCYv.jpg" alt="" width="100%">
+        <div class="banner-caption">
+            <div class="banner-call-plan"></div>
+            <div class="banner-value-plan">
+                <div class="limit-band"></div>
+                <div class="price-band">
+                    <div class="type-money"></div>
+                    <div class="description-value"></div>
+                    <div class="price"></div>
+                    <div class="zero-mounth"></div>
+                </div>
+                <div class="spotligth-channels">
+                    <div class="icon-channels"></div>
+                    <div class="icon-plus"></div>
+                    <div class="qtd-channels"></div>
+                </div>
+                <div class="button-banner">
+                    <button class="btn-banner">Contratar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,5 @@
 @extends('layout.template')
 
 @section('content')
-    <div class="section-main">
-        <h1>Body Site</h1>
-    </div>
+    @include('components._header')
 @endsection
