757d21f4aed84b5704e1d14ef5c2af974ef0bff8
Leandro Souza Da Silva <leandro.dasilvati7@gmail.com>
2019-04-03 19:15:36 -0300

    implements second banner

--- resources/views/components/_header.blade.php
+++ resources/views/components/_header.blade.php
@@ -1,6 +1,37 @@
 <header class="header-content">
-    <div class="banner-content">
-        <img src="https://i.imgur.com/9IhCCYv.jpg" alt="" width="100%">
+
+    <div class="content-bullet">
+        <label for="slide1"></label>
+        <input type="radio" name="slide" id="slide1">
+        <label for="slide2"></label>
+        <input type="radio" name="slide" id="slide2">
+    </div>
+
+    <div id="item-1" class="banner-content">
+        <img src="https://i.imgur.com/xj2UOAS.jpg" alt="">
+        <div class="banner-caption">
+            <div class="banner-call-plan"></div>
+            <div class="banner-value-plan">
+                <div class="limit-band"></div>
+                <div class="price-band">
+                    <div class="type-money"></div>
+                    <div class="description-value"></div>
+                    <div class="price"></div>
+                    <div class="zero-mounth"></div>
+                </div>
+                <div class="spotligth-channels">
+                    <div class="icon-channels"></div>
+                    <div class="icon-plus"></div>
+                    <div class="qtd-channels"></div>
+                </div>
+                <div class="button-banner">
+                    <button class="btn-banner">Contratar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="item-2" class="banner-content">
+        <img src="https://i.imgur.com/xj2UOAS.jpg" alt="">
         <div class="banner-caption">
             <div class="banner-call-plan"></div>
             <div class="banner-value-plan">
