094fdf89994a52e485ae90b7f794a1118ac551ce
Leandro Souza Da Silva <leandro.dasilvati7@gmail.com>
2019-04-08 19:16:14 -0300

    finishing developing section benefits

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -194,8 +194,8 @@
                 <div class="call-section-digital-services">
                     <div class="title-main">Conheça nossos <strong class="title-spotlight">serviços digitais</strong></div>
                     <p class="description-call">
-                        Além da nossa internet fibra de alta qualidade, você cliente TIM também desfruta de alguns dos nosso serviços
-                        digitais como canais online e aplicativos exclusivos para você.
+                        Ligações DDD regional e longa distância, nacional e internacional, já incluso no seu plano, sem
+                        surpresa na sua conta no final do mês.
                     </p>
                     <span class="line-bottom"></span>
                 </div>
@@ -208,10 +208,10 @@
                     </div>
                     <div class="content-show-channels">
                         <div class="monitor-channels">
-                            <img src="https://i.imgur.com/vhY2XXk.png" alt="">
+                            <img src="https://i.imgur.com/aVMNG44.png" alt="">
                         </div>
                         <div class="tablet-channels">
-                            <img src="https://i.imgur.com/VoSqyOs.png" alt="">
+                            <img src="https://i.imgur.com/j9wcPLz.png" alt="">
                         </div>
                         <div class="cellphone-channels">
                             <img src="https://i.imgur.com/copuma5.png" alt="">
@@ -224,65 +224,63 @@
                     </div>
                     <div class="row-up">
                         <div class="power-wifi">
-                            <div class="box-title">
-                                <h3 class="title-app">Tim power wi-fi</h3>
-                            </div>
-                            <div class="box-description-app">
-                                Com o nosso amplificador de sinal a sua internet via wi-fi não sofrerá
-                                nenhuma perca ao navegador com dispositivos móveis.
-                            </div>
-                            <div class="footer-description-app">
+                            <div class="box-space">
+                                <div class="box-title">
+                                    <h3 class="title-app">Tim power wi-fi</h3>
+                                </div>
+                                <div class="box-description-app">
+                                    Com o nosso amplificador de sinal a sua internet via wi-fi não sofrerá
+                                    nenhuma perca ao navegador com dispositivos móveis.
+                                </div>
                                 <p class="plan-disponibility">
                                     Disponivel nos planos <span class="spotiligh">300mb, 500mb, 1gb, 2gb</span>
                                 </p>
                             </div>
                         </div>
                         <div class="tim-learn">
-                            <div class="box-title">
-                                <h3 class="title-app">Tim ensina</h3>
+                            <div class="box-space">
+                                <div class="box-title">
+                                    <h3 class="title-app">Tim ensina</h3>
+                                </div>
+                                <div class="box-description-app">
+                                    Conteúdo educativo para aprender de forma rápida. Uma plataforma
+                                    completa, com mais de 40 cursos, divididos em diversas áreas do conhecimento.
+                                </div>
+                                <p class="plan-disponibility">
+                                    Disponivel nos planos <span class="spotiligh">500mb, 1gb, 2gb</span>
+                                </p>
                             </div>
-                            <div class="box-description-app">
-                                Conteúdo educativo para aprender de forma rápida. Uma plataforma
-                                completa, com mais de 40 cursos, divididos em diversas áreas do conhecimento.
-                            </div>
-                            <p class="plan-disponibility">
-                                Disponivel nos planos <span class="spotiligh">500mb, 1gb, 2gb</span>
-                            </p>
                         </div>
-
                     </div>
                     <div class="row-bottom">
                         <div class="protect">
-                            <div class="box-title">
-                                <h3 class="title-app">Tim live protect</h3>
-                            </div>
-                            <div class="box-description-app">
-                                Seus dados protegidos. Aplicativos com 30GB para guardar fotos. Videos e contatos,
-                                sem gastar memória do celular.
-                            </div>
-                            <div class="footer-description-app">
+                            <div class="box-space">
+                                <div class="box-title">
+                                    <h3 class="title-app">Tim live protect</h3>
+                                </div>
+                                <div class="box-description-app">
+                                    Seus dados protegidos. Aplicativos com 30GB para guardar fotos. Videos e contatos,
+                                    sem gastar memória do celular.
+                                </div>
                                 <p class="plan-disponibility">
                                     Disponivel em <span class="spotiligh">Todos os planos</span>
                                 </p>
                             </div>
                         </div>
-                        <div class="vitual-library">
-                            <div class="box-title">
-                                <h3 class="title-app">Tim banca virtual</h3>
+                        <div class="virtual-library">
+                            <div class="box-space">
+                                <div class="box-title">
+                                    <h3 class="title-app">Tim banca virtual</h3>
+                                </div>
+                                <div class="box-description-app">
+                                    Acesse mais de 100 titulos direto do seu Tim. Extra, Folha, Istoé. Veja e muitos
+                                    outros sem consumir da sua internet.
+                                </div>
+                                <p class="plan-disponibility">
+                                    Disponivel nos planos <span class="spotiligh">40mb, 100mb, 500mb, 1gb, 2gb</span>
+                                </p>
                             </div>
-                            <div class="box-description-app">
-                                Acesse mais de 100 titulos direto do seu Tim. Extra, Folha, Istoé. Veja e muitos
-                                outros sem consumir da sua internet.
-                            </div>
-                            <p class="plan-disponibility">
-                                Disponivel nos planos <span class="spotiligh">40mb, 100mb, 500mb, 1gb, 2gb</span>
-                            </p>
                         </div>
-
-                    </div>
-                    <div class="row-bottom">
-                        <div class="protect"></div>
-                        <div class="virtual-library"></div>
                     </div>
                 </div>
             </div>
@@ -290,9 +288,74 @@
     </section>
     <section class="benefits">
         <div class="content-benefits">
+            <div class="benefits-header">
+                <div class="call-section-benefits">
+                    <div class="title-main">Mais alguns outros <strong class="title-spotlight">benefícios</strong> para você</div>
+                    <p class="description-call">
+                        Além da nossa internet fibra de alta qualidade, você cliente TIM também desfruta de alguns dos nosso serviços
+                        digitais como canais online e aplicativos exclusivos para você.
+                    </p>
+                    <span class="line-bottom"></span>
+                </div>
+            </div>
 
             <div class="benefits-body">
+                <div class="benefits-box-local">
+                    <div class="box-local-space">
+                        <div class="box-local-header">
+                            <span class="icon-phone"><i class="fas fa-phone-volume"></i></span>
+                            <span class="icon-arrow"><i class="fas fa-arrow-right"></i></span>
+                            <span class="icon-phone-2"><i class="fas fa-phone"></i></span>
+                        </div>
+                        <div class="box-local-footer">
+                            <p class="description-plan-call">Voz ilimitado local</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="benefits-box-local-hover">
+                    <div class="box-local-space">
+                        <div class="box-local-header-hover"></div>
+                        <div class="box-local-footer-hover"></div>
+                    </div>
+                </div>
 
+                <div class="benefits-box-national">
+                    <div class="box-national-space">
+                        <div class="box-national-header">
+                            <span class="icon-phone"><i class="fas fa-phone-volume"></i></span>
+                            <span class="icon-brazil"><i class="fas fa-arrow-right"></i></span>
+                            <span class="icon-arrow"><img src="https://i.imgur.com/hzYveca.png" alt=""></span>
+                        </div>
+                        <div class="box-national-footer">
+                            <p class="description-plan-call">Voz ilimitado brasil</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="benefits-box-national-hover">
+                    <div class="box-national-space">
+                        <div class="box-national-header-hover"></div>
+                        <div class="box-national-footer-hover"></div>
+                    </div>
+                </div>
+
+                <div class="benefits-box-international">
+                    <div class="box-space-international">
+                        <div class="box-international-header">
+                            <span class="icon-phone"></span>
+                            <span class="icon-arrow"></span>
+                            <span class="icon-brazil"></span>
+                        </div>
+                        <div class="box-international-footer">
+                            <p class="description-plan-call">Voz ilimitado br + ldi</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="benefits-box-international-hover">
+                    <div class="box-international-space">
+                        <div class="box-international-header-hover"></div>
+                        <div class="box-international-footer-hover"></div>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
