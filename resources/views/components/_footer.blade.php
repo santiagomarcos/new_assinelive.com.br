ee158661ac50cb18423acb95d1425dd6aa3478a0
Leandro Souza Da Silva <leandro.dasilvati7@gmail.com>
2019-04-10 11:55:04 -0300

    finishing after footer

--- resources/views/components/_footer.blade.php
+++ resources/views/components/_footer.blade.php
@@ -1,22 +1,23 @@
 <footer class="content-footer">
-    <div class="box-left">
-        <h2 class="title-box-footer">Perguntas frequentes ?</h2>
+    <div class="container-footer">
+        <div class="box-left">
+            <h2 class="title-box-footer">Perguntas frequentes ?</h2>
 
-        <ul class="content-question">
-            <a href="#"><li class="items-ask">Como saber os canais no meu plano ?</li></a>
-            <a href="#"><li class="items-ask">Como acesso os canais online para assistir a programação ?</li></a>
-            <a href="#"><li class="items-ask">Já tenho um número de telefone e quero manter o mesmo ?</li></a>
-        </ul>
-    </div>
-    <div class="box-rigth">
-        <h2 class="title-box-footer">Siga-nos nas redes sociais</h2>
-        <div class="icons-redes">
-            <i class="fab fa-twitter"></i>
-            <i class="fab fa-facebook-f"></i>
-            <i class="fab fa-instagram"></i>
+            <ul class="content-question">
+                <a href="#"><li class="items-ask">Como saber os canais no meu plano ?</li></a>
+                <a href="#"><li class="items-ask">Como acesso os canais online para assistir a programação ?</li></a>
+                <a href="#"><li class="items-ask">Já tenho um número de telefone e quero manter o mesmo ?</li></a>
+            </ul>
+        </div>
+        <div class="box-rigth">
+            <h2 class="title-box-footer">Siga-nos nas redes sociais</h2>
+            <div class="icons-redes">
+                <i class="fab fa-twitter"></i>
+                <i class="fab fa-facebook-f"></i>
+                <i class="fab fa-instagram"></i>
+            </div>
         </div>
     </div>
-
     <div class="content-after">
         <div class="coopright">@ 2019 Assine Live. Todos os direitod reservados</div>
         <div class="developing">Desenvolvido por <a href="#">FFXSistemas</a></div>
