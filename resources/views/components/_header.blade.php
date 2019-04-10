9106191c05adfb2648968f3cf2ae3eb131b89e19
Leandro Souza Da Silva <leandro.dasilvati7@gmail.com>
2019-04-10 19:20:52 -0300

    finishing esctruture of site anda banner carousel

--- resources/views/components/_header.blade.php
+++ resources/views/components/_header.blade.php
@@ -7,48 +7,73 @@
         <input type="radio" name="slide" id="slide2">
     </div>
 
-    <div id="item-1" class="banner-content">
-        <img src="https://i.imgur.com/IUTvUup.jpg" alt="">
-        <div class="banner-caption">
-            <div class="banner-call-plan"></div>
-            <div class="banner-value-plan">
-                <div class="limit-band"></div>
-                <div class="price-band">
-                    <div class="type-money"></div>
-                    <div class="description-value"></div>
-                    <div class="price"></div>
-                    <div class="zero-mounth"></div>
-                </div>
-                <div class="spotligth-channels">
-                    <div class="icon-channels"></div>
-                    <div class="icon-plus"></div>
-                    <div class="qtd-channels"></div>
-                </div>
-                <div class="button-banner">
-                    <button class="btn-banner">Contratar</button>
+    <div class="slide-content">
+        <div class="banner-content bg1">
+            {{--<img src="https://i.imgur.com/IUTvUup.jpg" alt="">--}}
+            <div class="banner-caption">
+                <div class="banner-call-plan"></div>
+                <div class="banner-value-plan">
+                    <div class="limit-band"></div>
+                    <div class="price-band">
+                        <div class="type-money"></div>
+                        <div class="description-value"></div>
+                        <div class="price"></div>
+                        <div class="zero-mounth"></div>
+                    </div>
+                    <div class="spotligth-channels">
+                        <div class="icon-channels"></div>
+                        <div class="icon-plus"></div>
+                        <div class="qtd-channels"></div>
+                    </div>
+                    <div class="button-banner">
+                        <button class="btn-banner">Contratar</button>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div id="item-2" class="banner-content">
-        <img src="https://i.imgur.com/xj2UOAS.jpg" alt="">
-        <div class="banner-caption">
-            <div class="banner-call-plan"></div>
-            <div class="banner-value-plan">
-                <div class="limit-band"></div>
-                <div class="price-band">
-                    <div class="type-money"></div>
-                    <div class="description-value"></div>
-                    <div class="price"></div>
-                    <div class="zero-mounth"></div>
+        <div class="banner-content bg2">
+            {{--<img src="https://i.imgur.com/xj2UOAS.jpg" alt="">--}}
+            <div class="banner-caption">
+                <div class="banner-call-plan"></div>
+                <div class="banner-value-plan">
+                    <div class="limit-band"></div>
+                    <div class="price-band">
+                        <div class="type-money"></div>
+                        <div class="description-value"></div>
+                        <div class="price"></div>
+                        <div class="zero-mounth"></div>
+                    </div>
+                    <div class="spotligth-channels">
+                        <div class="icon-channels"></div>
+                        <div class="icon-plus"></div>
+                        <div class="qtd-channels"></div>
+                    </div>
+                    <div class="button-banner">
+                        <button class="btn-banner">Contratar</button>
+                    </div>
                 </div>
-                <div class="spotligth-channels">
-                    <div class="icon-channels"></div>
-                    <div class="icon-plus"></div>
-                    <div class="qtd-channels"></div>
-                </div>
-                <div class="button-banner">
-                    <button class="btn-banner">Contratar</button>
+            </div>
+        </div>
+        <div class="banner-content bg3">
+            {{--<img src="https://i.imgur.com/xj2UOAS.jpg" alt="">--}}
+            <div class="banner-caption">
+                <div class="banner-call-plan"></div>
+                <div class="banner-value-plan">
+                    <div class="limit-band"></div>
+                    <div class="price-band">
+                        <div class="type-money"></div>
+                        <div class="description-value"></div>
+                        <div class="price"></div>
+                        <div class="zero-mounth"></div>
+                    </div>
+                    <div class="spotligth-channels">
+                        <div class="icon-channels"></div>
+                        <div class="icon-plus"></div>
+                        <div class="qtd-channels"></div>
+                    </div>
+                    <div class="button-banner">
+                        <button class="btn-banner">Contratar</button>
+                    </div>
                 </div>
             </div>
         </div>
