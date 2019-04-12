4e3c158399133e2d13b24907f96d78d4d399adb4
Leandro Souza Da Silva <leandro.dasilvati7@gmail.com>
2019-04-12 13:49:14 -0300

    finhishing developing slider banner

--- resources/views/components/_header.blade.php
+++ resources/views/components/_header.blade.php
@@ -11,7 +11,7 @@
         {{--wathcing series--}}
         <div class="banner-content bg1">
             <div class="banner-caption cap1 ">
-                <div class="banner-call-plan">Aproveite as melhores séries e filmes com a nossa <strong>internet fibra.</strong> </div>
+                <div class="banner-call-plan">Aproveite as melhores séries e filmes com a nossa <strong class="spoligth-cap1">internet fibra.</strong> </div>
                 <div class="banner-value-plan">
                     <div class="content-left">
                         <div class="limit-band">60</div>
@@ -44,8 +44,7 @@
             {{--<img src="https://i.imgur.com/xj2UOAS.jpg" alt="">--}}
             <div class="banner-caption cap2">
                 <div class="banner-call-plan">
-                    Jogar online agora será sensacional, esteja entre os
-                    tops dos rankings nos E-sports conectado com a TIM Fibra.
+                    Esteja entre os tops dos rankings nos E-sports conectado com a <strong class="spoligth-cap2">tim fibra</strong>.
                 </div>
                 <div class="banner-value-plan">
                     <div class="content-left">
@@ -62,7 +61,7 @@
                         </div>
                     </div>
                     <div class="spotligth-channels">
-                        <div class="icon-channels"><img src="https://i.imgur.com/UPQVCmB.png" alt=""></div>
+                        <div class="icon-channels"><img src=https://i.imgur.com/3LTHDVO.png alt=""></div>
                         <div class="icon-plus">+</div>
                         <div class="qtd-channels">
                             <div class="channels-number">2</div>
@@ -76,31 +75,30 @@
             </div>
         </div>
         <div class="banner-content bg3">
-            {{--<img src="https://i.imgur.com/xj2UOAS.jpg" alt="">--}}
-            <div class="banner-caption cap2">
+            <div class="banner-caption cap3">
                 <div class="banner-call-plan">
-                    Jogar online agora será sensacional, esteja entre os
-                    tops dos rankings nos E-sports conectado com a TIM Fibra.
+                    Os mais diversos tipos de programação,
+                    incluído no nosso plano de <strong class="spoligth-cap3">Internet fibra </strong>.
                 </div>
                 <div class="banner-value-plan">
                     <div class="content-left">
-                        <div class="limit-band">100</div>
+                        <div class="limit-band">150</div>
                         <div class="desc-band">mega</div>
                     </div>
                     <div class="type-money">R$</div>
                     <div class="price-band">
-                        <div class="description-value">de <s>R$115,00</s> por </div>
-                        <div class="price">117</div>
+                        <div class="description-value">de <s>R$160,00</s> por </div>
+                        <div class="price">120</div>
                         <div class="zero-mounth">
                             <div class="zero">,00</div>
                             <div class="mounth">/mês</div>
                         </div>
                     </div>
                     <div class="spotligth-channels">
-                        <div class="icon-channels"><img src="https://i.imgur.com/UPQVCmB.png" alt=""></div>
+                        <div class="icon-channels"><img src="https://i.imgur.com/2c2HXuv.png" alt=""></div>
                         <div class="icon-plus">+</div>
                         <div class="qtd-channels">
-                            <div class="channels-number">2</div>
+                            <div class="channels-number">12</div>
                             <div class="channels-desc">canais online</div>
                         </div>
                     </div>
