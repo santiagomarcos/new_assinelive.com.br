e034d8132b0ebddd142d8df3eed56d137015cc61
Leandro Souza Da Silva <leandro.dasilvati7@gmail.com>
2019-04-18 12:24:26 -0300

    finishing developing menu view mobile

--- resources/views/components/_menu-responsive.blade.php
+++ resources/views/components/_menu-responsive.blade.php
@@ -1,13 +1,22 @@
 <div class="menu-responsive-content">
-    <div class="content-channels-button-close"><span>X</span></div>
+    <div class="content-channels-button-close"><span class="far fa-times"></span></div>
     <div class="content-channels-sales-responsive">
-        <div class="title-channels-responsive">Canais de atendimento:</div>
+        <div class="title-channels-responsive">Canais de atendimento</div>
         <ul class="content-channels-responsive">
-            <li class="items-channels-responsive">(11) 4890-9324</li>|
-            <li class="items-channels-responsive">Whatsapp</li>|
+            {{--<li class="items-channels-responsive">(11) 4890-9324</li>|--}}
+            <li class="items-channels-responsive">Whatsapp</li> |
             <li class="items-channels-responsive">Me ligue</li>
             {{--<li class="items-channels-responsive">Chat Online</li>--}}
         </ul>
         {{--<span class="location"><i class="fas fa-map-marker-alt"></i>São Paulo</span>--}}
+    </div>
+    <span class="line-bottom-responsive"></span>
+    <div class="content-items-menu">
+        <div class="title-menu-responsive">Menu</div>
+        <ul class="content-menu-responsive">
+            <a href="#"><li class="items-menu-responsive">Beneficios</li></a>
+            <a href="#"><li class="items-menu-responsive">Canais digitais</li></a>
+            <a href="#"><li class="items-menu-responsive">FAQ</li></a>
+        </ul>
     </div>
 </div>
