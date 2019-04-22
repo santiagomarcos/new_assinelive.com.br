1f4bf2d7f21afe0cc7fb88f56a09a04a08100578
Marcos Santiago <santiago@ffxsistemas.com>
2019-04-22 14:55:36 -0300

    created the new route for super list

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,11 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::group(['prefix' => 'v1', 'as' => 'v1.'], function(){
+    Route::group(['prefix' => 'consult', 'as' => 'consult.'], function() {
+        Route::post('/superlist/search', ['uses' => 'API\\SuperListController@searchAddress', 'as' => 'search']);
+    });
+});
+
