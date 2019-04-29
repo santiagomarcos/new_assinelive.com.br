40e71c3c27455b4997a7802376d86d99284e1c49
Marcos Santiago <santiago@ffxsistemas.com>
2019-04-29 16:20:13 -0300

    created the model for lead viability

---
+++ app/Models/LeadViability.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Marcos Santiago
+ * Date: 29/04/2019
+ * Time: 12:06
+ */
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class LeadViability extends Model
+{
+    protected $guarded = [];
+    protected $table = 'lead_viability';
+    public $timestamps = true;
+
+}
