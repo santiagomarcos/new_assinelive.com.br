f50a0cc3898a3d6e70aae57dbad0b2b0258f5c59
Marcos Santiago <santiago@ffxsistemas.com>
2019-05-03 11:46:21 -0300

    adjust css

--- resources/views/layout/admin.blade.php
+++ resources/views/layout/admin.blade.php
@@ -5,8 +5,8 @@
     <title>Assine Live - Administração</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Fonts -->
-    <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:300,400' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,900' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Condensed:300,400' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Lato:300,400,700,900' rel='stylesheet' type='text/css'>
 
     <!-- CSS App -->
     <link rel="stylesheet" type="text/css" href="/css/admin.css">
