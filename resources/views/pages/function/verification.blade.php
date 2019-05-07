90546bbdc1bade3664efd6c1f9b3fe4e4c8bc14a
Leandro Souza Da Silva <leandro.dasilvati7@gmail.com>
2019-05-07 17:42:11 -0300

    finishing modal-wttx '

--- resources/views/pages/function/verification.blade.php
+++ resources/views/pages/function/verification.blade.php
@@ -37,6 +37,7 @@
 @section('content')
     {{--@include('components._header')--}}
     @include('components._modal_calling')
+    @include('components._modal-wttx')
     <div id="main-verification">
         <section id="section-verification">
             <div class="content-verification">
@@ -297,6 +298,24 @@
         }
 
         $('document').ready(function () {
+
+            $('#owl_wttx.owl-carousel').owlCarousel({
+                margin:10,
+                loop:false,
+                nav:true,
+                dots:false,
+                responsive:{
+                    0:{
+                        items:1
+                    },
+                    600:{
+                        items:1
+                    },
+                    1000:{
+                        items:2
+                    }
+                }
+            });
 
             function inputEmpty(){
                 if ($('#name').val() == "" ||
