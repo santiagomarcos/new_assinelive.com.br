8430f77e0f81ad8fe3fd001516bb8e0a0a82967e
Marcos Santiago <santiago@ffxsistemas.com>
2020-04-01 18:38:11 -0300

    Revert "fix"

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Agente Autorizado - Administração</title>
+    <title>Assine Live - Administração</title>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
           integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
@@ -164,7 +164,8 @@
         </div>
         <div class="logo">
             <a href="http://localhost:8000" class="brand">
-                <img src="https://i.imgur.com/GgbsK5n.png">
+                <p class="brand-firstname"><b>A</b>ssine live</p>
+                <p class="brand-secondname">Administração</p>
             </a>
         </div>
         <div class="form-content" style="margin-left: 0px; margin-right: 0px;">
