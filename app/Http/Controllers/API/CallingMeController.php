410659cae395849c97aed8fc29dc62d001fb47fb
Marcos Santiago <santiago@ffxsistemas.com>
2020-05-06 17:13:53 -0300

    fix

--- app/Http/Controllers/API/CallingMeController.php
+++ app/Http/Controllers/API/CallingMeController.php
@@ -11,10 +11,15 @@ namespace App\Http\Controllers\API;
 
 use App\Models\CallingMe;
 use App\Repositories\CallingMeRespository;
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 
 class CallingMeController
 {
+    public function __construct()
+    {
+        $this->client = (new Client());
+    }
 
     public function create(Request $request)
     {
@@ -22,6 +27,12 @@ class CallingMeController
         $message = (new CallingMeRespository())->create([
             'name' => strtoupper($args['name']),
             'number_phone' => $args['phone'],
+        ]);
+        $vowels = array("(", ")", "-", " ",);
+        $onlyconsonants = str_replace($vowels, "", $args['phone']);
+
+        $res = $this->client->request('POST', 'http://newtimesp.ddns.net:8003/contact/digital?contact_name='.$args['name'].'&queue=digital&billing_group_id=1&to[1]='.$onlyconsonants, [
+            'auth' => ["tabuladorweb", "ij29Kg9QooK77cl"]
         ]);
     }
 }
