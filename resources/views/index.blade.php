7179797c7723a04f61af8e791644ef807692cf56
Marcos Santiago <santiago@ffxsistemas.com>
2020-05-18 20:30:44 -0300

    fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -445,5 +445,14 @@
             });
         });
     </script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-167041002-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-167041002-1');
+    </script>
+
 
 @endsection
