ea036c8b8b312d42231f20d67667adda03415df2
Marcos Santiago <santiago@ffxsistemas.com>
2020-06-10 09:17:58 -0300

    added the sitemap

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,64 @@
 |
 */
 
+Route::get('/sitemap.xml', function(){
+
+$variabel ="<?xml version='1.0' encoding='UTF-8'?>
+<urlset
+      xmlns='http://www.sitemaps.org/schemas/sitemap/0.9'
+      xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance'
+      xsi:schemaLocation='http://www.sitemaps.org/schemas/sitemap/0.9
+            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd'>
+<!-- created with Free Online Sitemap Generator www.xml-sitemaps.com -->
+
+
+<url>
+  <loc>https://planotimlive.com.br/</loc>
+  <lastmod>2020-06-08T17:22:33+00:00</lastmod>
+  <priority>1.00</priority>
+</url>
+<url>
+  <loc>https://planotimlive.com.br/pages/faq</loc>
+  <lastmod>2020-06-08T17:22:33+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>https://planotimlive.com.br/pages/verifications/PLAN60M</loc>
+  <lastmod>2020-06-08T17:22:33+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>https://planotimlive.com.br/pages/verifications/PLAN150M</loc>
+  <lastmod>2020-06-08T17:22:33+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>https://planotimlive.com.br/pages/verifications/PLAN200M</loc>
+  <lastmod>2020-06-08T17:22:33+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>https://planotimlive.com.br/pages/verifications/PLAN300M</loc>
+  <lastmod>2020-06-08T17:22:33+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>https://planotimlive.com.br/pages/verifications/40</loc>
+  <lastmod>2020-06-08T17:22:33+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>https://planotimlive.com.br/pages/regulaments</loc>
+  <lastmod>2020-06-08T17:22:33+00:00</lastmod>
+  <priority>0.80</priority>
+</url>
+</urlset>";
+
+   return response()->make($variabel, 200, [
+        'Content-Type' => 'application/xml'
+    ]);
+});
+
 Route::group(['middleware' => 'analytics'], function () {
     Route::get('/', 'PagesController@home')->name('home');
     Route::group(['prefix' => 'pages', 'as' => 'pages.'], function () {
